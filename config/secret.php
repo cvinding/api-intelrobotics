735b36512a89c63e1bec929a6d27c3f0cd554391
czvr0 <chri656v@edu.sde.dk>
2019-03-18 13:57:35 +0100

    cleanup

--- config/secret.php
+++ config/secret.php
@@ -1,5 +1,5 @@
 <?php
 // Generated with base64_encode(openssl_random_pseudo_bytes(32))
-// TODO: move this file to a safer location
+// TODO: move this file to a safer location maybe home folder of root
 
 return "B!8ryX7RF*h9Z1@^Sn#@1m6S*ymh7$7n";
