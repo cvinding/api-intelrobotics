21625067e33861f7a413ddcb8683231de908be98
czvr0 <chri656v@edu.sde.dk>
2019-04-04 14:40:59 +0200

    usercontroller

--- controller/userController.php
+++ controller/userController.php
@@ -28,6 +28,47 @@ class UserController extends Controller implements \CONTROLLER\_IMPLEMENTS\Contr
     public function searchUserList(string $searchInput) {
         //TODO: used by HR to search for user + CPR number
 
+
+        /*
+        $username = "API_LDAP_SEARCHER";
+        $password = "IntelRobotics!";
+
+        $hostname = "ldap://ad.intelrobotics.dk";
+
+        $ldapConn = ldap_connect($hostname);
+
+        $ldapRDN = 'AD' . "\\" . $username;
+
+        ldap_set_option($ldapConn, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldapConn, LDAP_OPT_X_TLS_REQUIRE_CERT, 0);
+        ldap_set_option($ldapConn, LDAP_OPT_REFERRALS, 0);
+
+        //TODO: create fronter like
+        if(!ldap_start_tls($ldapConn)) {
+            var_dump("LDAP TLS connection failed");
+        }
+
+        $ldapBind = ldap_bind($ldapConn, $ldapRDN, $password);
+
+        if($ldapBind) {
+
+            $ldapBaseDN = "OU=IntelRobotics,DC=ad,DC=intelrobotics,DC=dk";
+            $search = "(&(objectCategory=person))";
+            $result = ldap_search($ldapConn, $ldapBaseDN, $search);
+
+            $entries = ldap_get_entries($ldapConn, $result);
+
+            //var_dump($entries);
+
+            //TODO: check other memberof entries for ServiceUser_SG
+            foreach($entries as $entry) {
+                if(strpos($entry["givenname"][0], "Template") === false && strpos($entry["memberof"][0], "ServiceUser_SG") === false) {
+                  var_dump($entry);
+                }
+            }
+        }
+
+        */
         exit(json_encode(["result" => $searchInput, "status" => true]));
     }
 
